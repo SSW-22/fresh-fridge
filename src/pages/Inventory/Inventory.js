dbf67f94965fa57ac47b7c53e5bbc77e7f32c72f
Seungmin <shin891211@gmail.com>
2022-11-30 11:39:53 -0500

    commit for update from main

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,5 +1,17 @@
+import AddItem from "../../components/global/buttons/AddItem";
+import classes from "./Inventory.module.css";
+
 function Inventory() {
-  return <div data-testid="inventory-component">Inventory Component</div>;
+  return (
+    <div data-testid="inventory-component" className={classes.container}>
+      <div>Search bar</div>
+      <h1>
+        There is no food item here. <br /> click on the add item button to store
+        food.
+      </h1>
+      <AddItem />
+    </div>
+  );
 }
 
 export default Inventory;
