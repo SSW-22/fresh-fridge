e6b64d808dba2616d8029dfbb7250e775a622f9b
Woong O <woong0419@gmail.com>
2022-12-13 01:48:33 -0500

    resolved #38: delete btn

--- src/components/editDeleteMove/__test__/EditMoveDeleteBtn.test.js
+++ src/components/editDeleteMove/__test__/EditMoveDeleteBtn.test.js
@@ -1,7 +1,12 @@
 import { fireEvent, screen } from "@testing-library/react";
 import EditMoveDelete from "../EditMoveDelete";
 import renderWithProviders from "../../../utils/test-utils";
+import { groceryItems } from "../../../utils/dummyInventoryStore";
 
+const initialItems = {
+  userId: "test",
+  items: groceryItems,
+};
 describe("edit move delete wrapper", () => {
   it("render three btns", () => {
     renderWithProviders(<EditMoveDelete />);
@@ -16,5 +21,25 @@ describe("edit move delete wrapper", () => {
     fireEvent.click(screen.getByRole("button", { name: "Edit" }));
 
     expect(await screen.findByText("Edit item")).toBeInTheDocument();
+  });
+
+  it("render delete form when delete btn is clicked", async () => {
+    renderWithProviders(<EditMoveDelete selectedId="1" />);
+    fireEvent.click(screen.getByRole("button", { name: "Delete" }));
+
+    expect(await screen.findByText("Delete item")).toBeInTheDocument();
+  });
+
+  it("close delete form when close btn is clicked", async () => {
+    renderWithProviders(<EditMoveDelete selectedId="1" />, {
+      preloadedState: {
+        inventory: initialItems,
+      },
+    });
+    fireEvent.click(screen.getByRole("button", { name: "Delete" }));
+    expect(await screen.findByText("Delete item")).toBeInTheDocument();
+
+    fireEvent.click(screen.getByTestId("close-btn"));
+    expect(screen.queryByText("Delete item")).not.toBeInTheDocument();
   });
 });
