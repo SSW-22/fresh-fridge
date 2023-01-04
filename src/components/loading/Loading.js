6ca5b17ebf2415bd8d1f7ed3480bd47d70841f55
Woong O <woong0419@gmail.com>
2023-01-03 19:13:05 -0500

    resolved #67: loading spinner

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,7 +1,12 @@
 import classes from "./Loading.module.css";
 
 function Loading() {
-  return <div className={classes.loading}>Loading</div>;
+  return (
+    <div className={classes.loading}>
+      <div className={classes["loading-spinner"]} />
+      Loading
+    </div>
+  );
 }
 
 export default Loading;
